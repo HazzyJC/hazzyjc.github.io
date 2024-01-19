2786b9ab52a6053494b481f48c34878ae72d0f9a
HazzyGG <94419858+HazzyGG@users.noreply.github.com>
2024-01-18 19:33:52 -0800

    Final commit probably

--- script.js
+++ script.js
@@ -4,23 +4,20 @@
           hour = minute * 60,
           day = hour * 24;
   
-    //I'm adding this section so I don't have to keep updating this pen every year :-)
-    //remove this if you don't need it
     let today = new Date(),
         dd = String(today.getDate()).padStart(2, "0"),
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "09/30/",
-        birthday = dayMonth + yyyy;
+        dayMonth = "09/27/",
+        twoyearend = dayMonth + 2024;
     
     today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-      birthday = dayMonth + nextYear;
+    if (today > twoyearend) {
+      twoyearend = dayMonth;
     }
     //end
     
-    const countDown = new Date(birthday).getTime(),
+    const countDown = new Date(twoyearend).getTime(),
         x = setInterval(function() {    
   
           const now = new Date().getTime(),
@@ -33,11 +30,11 @@
   
           //do something later when date is reached
           if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("headline").innerText = "it's joever.";
             document.getElementById("countdown").style.display = "none";
             document.getElementById("content").style.display = "block";
             clearInterval(x);
           }
           //seconds
         }, 0)
-    }());
+    }())
